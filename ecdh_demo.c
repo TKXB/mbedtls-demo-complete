b3c689dde192825b41befcb25daa284861984c1a
root <wang3919379@163.com>
2019-02-25 02:31:15 +0000

    update ecdh demo

--- ecdh_demo.c
+++ ecdh_demo.c
@@ -44,15 +44,15 @@ int main( void )
 int main(int argc, char *argv[]) {
     //First method: Generate keypair each time
     size_t olen, olen2;
-    unsigned char buf[256];
-    unsigned char buf2[256];
+    unsigned char buf[32];
+    unsigned char buf2[32];
 
     int ret = 1;
     int exit_code = MBEDTLS_EXIT_FAILURE;
     mbedtls_ecdh_context ctx_cli, ctx_srv;
     mbedtls_entropy_context entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
-    unsigned char cli_to_srv[32], srv_to_cli[32];
+    unsigned char cli_to_srv[70], srv_to_cli[70];
     const char pers[] = "ecdh";
     ((void) argc);
     ((void) argv);
@@ -77,23 +77,27 @@ int main(int argc, char *argv[]) {
 
     ret = mbedtls_ecdh_setup(&ctx_cli, MBEDTLS_ECP_DP_SECP256R1);
     ret = mbedtls_ecdh_setup(&ctx_srv, MBEDTLS_ECP_DP_SECP256R1);
-    ret = mbedtls_ecdh_make_params(&ctx_srv, &olen, buf, sizeof buf, mbedtls_ctr_drbg_random, &ctr_drbg);
+
+    //Server generates an EC key pair and export its public key to the buffer
+    //mbedtls_ecdh_make_params vs mbedtls_ecdh_make_public: mbedtls_ecdh_make_params contains groupid
+    ret = mbedtls_ecdh_make_params(&ctx_srv, &olen, srv_to_cli, sizeof srv_to_cli, mbedtls_ctr_drbg_random, &ctr_drbg);
     printf("ret make params = %x\n", -ret);
     printf("client read params\n");
     // client read params and make public params
-    const unsigned char * buf_ptr = buf;
+    const unsigned char * buf_ptr = srv_to_cli;
     ret = mbedtls_ecdh_read_params(&ctx_cli, &buf_ptr, buf_ptr+olen);
+
 
     printf("ret read params = %x\n", -ret);
     printf("client make public\n");
-    ret = mbedtls_ecdh_make_public(&ctx_cli, &olen, buf, 256, mbedtls_ctr_drbg_random, &ctr_drbg);
-    ret = mbedtls_ecdh_calc_secret(&ctx_cli, &olen2, buf2, 256, mbedtls_ctr_drbg_random, &ctr_drbg);
+    //Client generates an EC key pair and export its public key to the buffer
+    ret = mbedtls_ecdh_make_public(&ctx_cli, &olen, cli_to_srv, sizeof cli_to_srv, mbedtls_ctr_drbg_random, &ctr_drbg);
+    ret = mbedtls_ecdh_calc_secret(&ctx_cli, &olen2, buf2, 32, mbedtls_ctr_drbg_random, &ctr_drbg);//generates Masterkey
     printf("ret make public = %x\n", -ret);
     // server read public and calc secret
     printf("server read and calc\n");
-    ret = mbedtls_ecdh_read_public(&ctx_srv, buf, olen);
-    for(size_t i = 0; i < 5; i++)
-        ret = mbedtls_ecdh_calc_secret(&ctx_srv, &olen, buf, 256, mbedtls_ctr_drbg_random, &ctr_drbg);
+    ret = mbedtls_ecdh_read_public(&ctx_srv, cli_to_srv, olen);
+    ret = mbedtls_ecdh_calc_secret(&ctx_srv, &olen, buf, 32, mbedtls_ctr_drbg_random, &ctr_drbg);
 
     printf("server secret key: 0x");
     for(size_t i = 0; i < olen; i++) {
@@ -108,7 +112,7 @@ int main(int argc, char *argv[]) {
     printf("\n");
 
 
-    //Another method: load keypair from file
+    //The second method: load keypair from file
     //note: Use ecc_key_gen to generate keypair file first
     unsigned char buf_file[256];
     unsigned char buf_file2[256];
@@ -144,6 +148,43 @@ int main(int argc, char *argv[]) {
     for(size_t i = 0; i < olen2; i++) {
         printf("%x", buf_file2[i]);
     }
+
+
+    //Third method: Simulate client and server interaction
+    unsigned char cli_to_srv_third[70], srv_to_cli_third[70], buf_third[32], buf2_third[32];
+    mbedtls_ecdh_context ctx_cli_third, ctx_srv_third;
+    mbedtls_pk_context pk_srv_third;
+    mbedtls_pk_init( &pk_srv_third );
+    mbedtls_ecdh_init( &ctx_srv_third );
+    mbedtls_ecdh_init( &ctx_cli_third );
+
+    mbedtls_pk_parse_keyfile( &pk_srv_third, "privatekey.txt", NULL );
+    mbedtls_pk_parse_public_keyfile( &pk_srv_third, "publickey.txt" );
+    ret = mbedtls_ecdh_setup(&ctx_srv_third, MBEDTLS_ECP_DP_SECP256R1);
+    ret = mbedtls_ecdh_setup(&ctx_cli_third, MBEDTLS_ECP_DP_SECP256R1);
+    ret = mbedtls_ecdh_get_params(&ctx_srv_third, mbedtls_pk_ec(pk_srv_third), MBEDTLS_ECDH_OURS);
+
+    ret = mbedtls_ecdh_make_public(&ctx_srv_third, &olen, srv_to_cli_third, sizeof srv_to_cli_third, mbedtls_ctr_drbg_random, &ctr_drbg);
+
+    const unsigned char * buf_ptr_third = srv_to_cli_third;
+    ret = mbedtls_ecdh_read_public(&ctx_cli_third, buf_ptr_third, olen);
+    ret = mbedtls_ecdh_make_public(&ctx_cli_third, &olen, cli_to_srv_third, sizeof cli_to_srv_third, mbedtls_ctr_drbg_random, &ctr_drbg);
+    ret = mbedtls_ecdh_calc_secret(&ctx_cli_third, &olen2, buf2_third, 32, mbedtls_ctr_drbg_random, &ctr_drbg);//generates Masterkey
+    ret = mbedtls_ecdh_read_public(&ctx_srv_third, cli_to_srv_third, olen);
+    ret = mbedtls_ecdh_calc_secret(&ctx_srv_third, &olen, buf_third, 32, mbedtls_ctr_drbg_random, &ctr_drbg);
+
+    printf("\n\nThird method:\n");
+    printf("server secret key: 0x");
+    for(size_t i = 0; i < olen; i++) {
+        printf("%x", buf_third[i]);
+    }
+    printf("\n");
+    printf("client secret key: 0x");
+    for(size_t i = 0; i < olen2; i++) {
+        printf("%x", buf2_third[i]);
+    }
+    printf("\n");
+    printf("\n");
     return 0;
 }
 
